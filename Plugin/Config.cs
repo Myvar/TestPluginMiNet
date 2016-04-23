b81d5fa3f0b9d9c4f51d96c81555ccffc5acbae2
Emile Badenhorst <MyvarHD@gmail.com>
2016-04-23 23:04:40 +0200

    init

---
+++ Plugin/Config.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPlugin
+{
+    public class Config
+    {
+        public bool Enabled { get; set; } = true;
+        public List<Message> Messages { get; set; } = new List<Message>()
+        {
+            new Message() { DelayInSconds = 60 * 60 , Text = "Example Message"}
+        };
+    }
+
+    public class Message
+    {
+        public string Text { get; set; }
+        public int DelayInSconds { get; set; }
+    }
+}
---
+++ Plugin/TestPlugin.cs
@@ -0,0 +1,66 @@
+﻿
+using MiNET;
+using MiNET.Net;
+using MiNET.Plugins;
+using MiNET.Plugins.Attributes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestPlugin
+{
+    [Plugin]
+    public class TestPlugin : Plugin
+    {
+        public bool Run { get; set; }
+        public Config cfg { get; set; } = new Config();
+
+        protected override void OnEnable()
+        {
+            Run = true;
+
+            Console.WriteLine("Test Plugin Started");
+
+            if (File.Exists("config.json"))
+            {
+                cfg = JsonConvert.DeserializeObject<Config>(File.ReadAllText("config.json"));
+            }
+            else
+            {
+                File.WriteAllText("config.json", JsonConvert.SerializeObject(cfg, Formatting.Indented));
+            }
+
+
+            foreach (var i in cfg.Messages)
+            {
+                ThreadPool.QueueUserWorkItem((p) =>
+                {
+                    while (Run && cfg.Enabled)
+                    {
+                        foreach (var z in Context.LevelManager.Levels)
+                        {
+                            z.BroadcastMessage(i.Text);
+                        }
+
+                        Thread.Sleep(1000 * i.DelayInSconds);
+                    }
+                });
+            }
+
+            base.OnEnable();
+        }
+
+        public override void OnDisable()
+        {
+            Run = false;
+            base.OnDisable();
+        }
+
+    }
+}
