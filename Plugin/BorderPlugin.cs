ca2f7b3c694cf08b299432b0f8251a565a815a9f
Emile Badenhorst <MyvarHD@gmail.com>
2016-04-24 12:49:10 +0200

    Added border plugin

---
+++ Plugin/BorderPlugin.cs
@@ -0,0 +1,56 @@
+﻿using MiNET;
+using MiNET.Net;
+using MiNET.Plugins;
+using MiNET.Plugins.Attributes;
+using MiNET.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestPlugin
+{
+    [Plugin]
+    public class BorderPlugin : Plugin
+    {
+        public int Boder = 10;
+
+        protected override void OnEnable()
+        {
+            Console.WriteLine("Boder Plugin Loaded");
+            base.OnEnable();
+        }
+
+        [PacketHandler, Receive]
+        public Package HandleIncomingMove(McpeMovePlayer packet, Player player)
+        {            
+            var spawn = player.Level.SpawnPoint;
+            if(packet.x <= spawn.X + Boder && packet.x >= spawn.X - Boder)
+            {
+                if (packet.z <= spawn.Z + Boder && packet.z >= spawn.Z - Boder)
+                {
+                    return packet;
+                }
+            }
+            
+           
+                player.SetPosition(new PlayerLocation(
+                    player.KnownPosition.X  - (packet.x - player.KnownPosition.X),
+                    player.KnownPosition.Y, 
+                    player.KnownPosition.Z - (packet.z - player.KnownPosition.Z)) {
+                    HeadYaw = player.KnownPosition.HeadYaw,
+                    Pitch = player.KnownPosition.Pitch,
+                    Yaw = player.KnownPosition.Yaw
+                });
+           
+
+            player.SendMessage("Out of bounce");
+            return packet;
+        }
+    }
+}
